21eda377f472bccb7f7150c8dcb91fd6f8a38ec0
Olga G <og.40785@gmail.com>
2024-12-28 22:19:55 +0100

    add: 38 test passing, 11 pending

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -387,8 +387,8 @@ function generateOdds(len) {
  *   getElementByIndices(['one','two','three'], [2]) => 'three'  (arr[2])
  *   getElementByIndices([[[ 1, 2, 3]]], [ 0, 0, 1 ]) => 2        (arr[0][0][1])
  */
-function getElementByIndices(/* arr, indices */) {
-  throw new Error('Not implemented');
+function getElementByIndices(arr, indices) {
+  return indices.reduce((storage, index) => storage[index], arr);
 }
 
 /**
@@ -403,8 +403,15 @@ function getElementByIndices(/* arr, indices */) {
  *  getFalsyValuesCount([ -1, 'false', null, 0 ]) => 2
  *  getFalsyValuesCount([ null, undefined, NaN, false, 0, '' ]) => 6
  */
-function getFalsyValuesCount(/* arr */) {
-  throw new Error('Not implemented');
+function getFalsyValuesCount(arr) {
+  const falsy = arr.reduce(function countFalsy(count, currentValue) {
+    if (!currentValue) {
+      return count + 1;
+    }
+    return count;
+  }, 0);
+
+  return falsy;
 }
 
 /**
@@ -472,8 +479,8 @@ function getHexRGBValues(/* arr */) {
  *   getMaxItems([ 10, 2, 7, 5, 3, -5 ], 3) => [ 10, 7, 5 ]
  *   getMaxItems([ 10, 10, 10, 10 ], 3) => [ 10, 10, 10 ]
  */
-function getMaxItems(/* arr, n */) {
-  throw new Error('Not implemented');
+function getMaxItems(arr, n) {
+  return arr.sort((a, b) => b - a).slice(0, n);
 }
 
 /**
